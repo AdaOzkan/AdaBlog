f26a01a5257c9960d94b52b3a8bd93577881e590
AdaOzkan <149889162+AdaOzkan@users.noreply.github.com>
2026-02-14 21:07:40 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB5C...",
+  apiKey: "AIzaSyB5CJpWgzNaXXXM2ekmFxrLzzq7gIg1gwM",
   authDomain: "adablog-9c169.firebaseapp.com",
   projectId: "adablog-9c169",
   storageBucket: "adablog-9c169.firebasestorage.app",
   messagingSenderId: "1038802296855",
   appId: "1:1038802296855:web:3e2fb416c3dcbb77c4bc52",
-  measurementId: "G-KCRCRHUTF3"
+  measurementId: "G-KCRCRHJTF3"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-document.getElementById("googleLogin").addEventListener("click", () => {
-  signInWithPopup(auth, provider)
-    .then(() => {
-      window.location.href = "home.html";
-    })
-    .catch(err => alert(err.message));
-});
-
